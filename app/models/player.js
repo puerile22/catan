c5bbe99273ea08adb2b8c83aa6ee2ef6215028cb
Jon Rogozen <jon.rogozen@gmail.com>
2014-11-11 14:34:56 -0800

    add player model

---
+++ app/models/player.js
@@ -0,0 +1,16 @@
+app.factory('Player', ['$resource', '$route', function($resource, $route) {
+
+  var models = {};
+
+  var settings = {
+
+  }
+
+
+
+  return {
+    models: models,
+    settings: settings
+  }
+
+}]);
